7e1f74c4b1ea8f7442693ddea4aa2434fe389d59
Kushagra Singh Bhadauria <156601936+Codykush@users.noreply.github.com>
2026-08-08 17:49:32 +0530

    Fix Vercel HTTPS API routing

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://3.110.136.211:8080",
+    baseURL: "/",
     headers: {
         "Content-Type": "application/json",
     },
